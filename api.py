126a31c07ab2b0a869dd1cb899a9c2f6b75b0288
sungongwei <273965695@qq.com>
2024-11-07 14:43:39 +0800

    fix:

--- api.py
+++ api.py
@@ -11,5 +11,5 @@ def qa(q):
     return answer
 
 if __name__ == '__main__':
-    app.run(debug=False,port=8000)
+    app.run(debug=False,host="0.0.0.0",port=8000)
     print('Server running on port 8000...')
