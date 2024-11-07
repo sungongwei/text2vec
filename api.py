608361cf99b2e57aa59749fe178d03264a783ecb
sungongwei <273965695@qq.com>
2024-11-07 10:29:09 +0800

    fix:

--- api.py
+++ api.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from src.base import answer_question
+from src.base import answer_question ,vectorize
 app = Flask(__name__)
 
 @app.route('/q/<q>')
@@ -9,7 +9,13 @@ def qa(q):
     answer = answer_question(q)
     # 返回答案
     return answer
-
+@app.route('/train/', methods=['POST'])
+def vector():
+    # 获取 POST 请求中的数据
+    data = request.json
+    vectorize(data)
+    # 返回答案
+    return jsonify({'code': 0})
 if __name__ == '__main__':
     app.run(debug=False,port=8000)
     print('Server running on port 8000...')
--- src/base.py
+++ src/base.py
@@ -5,12 +5,15 @@ import json
 import time
 import faiss
 import os
+from datetime import datetime
 from src.read_data import merge_json_files
 
 # 打开 JSON 文件并读取数据
 with open("config.json") as f:
     config = json.load(f)
 question_list = merge_json_files("./data")
+if os.path.exists("index") == False:
+    os.mkdir('index')
 
 
 print(f"\r加载模型...", end="", flush=True)
@@ -33,14 +36,21 @@ print("\n加载模型完成:", end - start, "seconds")
 
 d = 768
 
-def vectorize():
+def generate_index_path():
+    # 获取当前时间并格式化为字符串
+    current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
+    # 将时间戳添加到基础路径中
+    return f"index/{current_time}.index"
+
+def vectorize(data):
     index = faiss.IndexFlatL2(d)
+    global index_with_ids
     index_with_ids = faiss.IndexIDMap(index)
     # 1. 准备数据
     # 2. 对问题进行编码和向量化
     start = time.time()
-    question_length = len(question_list)
-    for idx, questions in enumerate(question_list):
+    question_length = len(data)
+    for idx, questions in enumerate(data):
         vectors = []
         print(f"\r向量化: {idx+1}/{question_length}", end="", flush=True)
         for question in questions["question"]:
@@ -57,10 +67,16 @@ def vectorize():
     print("\n向量化:", end - start, "seconds")
 
     print(index_with_ids.ntotal)
+    faiss.write_index(index_with_ids, index_path)
+    faiss.write_index(index_with_ids, generate_index_path())
+                      
+    global question_list
+    question_list= data
+    print(question_list)
     return index_with_ids
-  
 
 index_path = "faiss.index"
+index_with_ids = None
 # 检查索引文件是否存在
 if os.path.exists(index_path):
     # 如果文件存在，则加载本地索引
@@ -69,13 +85,14 @@ if os.path.exists(index_path):
 else:
     # 如果文件不存在，则创建一个新的索引
     print("Creating a new index...")
-    index_with_ids = vectorize()
+    vectorize(question_list)
     print("Saving index to file...")
-    faiss.write_index(index_with_ids, index_path)
+    
 
 
 
 def answer_question(user_input):
+    print(index_with_ids.ntotal)
     user_inputs = tokenizer(
         user_input, return_tensors="pt", max_length=512, padding=True, truncation=True
     ).to(device)
